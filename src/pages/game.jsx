9bfce65438829d89c1268f99f21114e152ae25e8
asror797 <asror797shahobiddinov@gmail.com>
2024-04-04 11:11:54 +0500

    car image configured

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -3,6 +3,10 @@ import './game.css'
 import Leaderboard from '../components/leaderboard/leaderboard'
 import Navbar from '../components/navbar/navbar'
 import Footer from '../components/footer/footer'
+import auto from '../assets/Auto_5.png'
+import auto1 from '../assets/Auto_7.png'
+import auto2 from '../assets/Auto_6.png'
+import auto3 from '../assets/Auto_8.png'
 
 const Game = () => {
   return(
@@ -13,9 +17,12 @@ const Game = () => {
           <div className="game-page-wrapper">
             <h1>Game page</h1>
             <div className="game-playground">
-            <div className="racer-wrapper">
+              <div className="racer-wrapper">
                 <div className="player-way">
-                  <p>Moshina</p>
+                <div className="player-car-wrapper">
+                    <div className="player-name">You (Guest)</div>
+                    <img height={40} src={auto}  alt="car" />
+                  </div>
                 </div>
                 <div className="player-speed">
                   45wpm
@@ -23,7 +30,10 @@ const Game = () => {
               </div>
               <div className="racer-wrapper">
                 <div className="player-way">
-                  <p>Moshina</p>
+                  <div className="player-car-wrapper">
+                    <div className="player-name">Babur (babur20)</div>
+                    <img height={40} src={auto1}  alt="car" />
+                  </div>
                 </div>
                 <div className="player-speed">
                   45wpm
@@ -31,7 +41,10 @@ const Game = () => {
               </div>
               <div className="racer-wrapper">
                 <div className="player-way">
-                  <p>Moshina</p>
+                  <div className="player-car-wrapper">
+                    <div className="player-name">Jamshid (Jamshid13)</div>
+                    <img height={40} src={auto2}  alt="car" />
+                  </div>
                 </div>
                 <div className="player-speed">
                   45wpm
@@ -39,7 +52,12 @@ const Game = () => {
               </div>
               <div className="racer-wrapper">
                 <div className="player-way">
-                  <p>Moshina</p>
+                <div className="player-car-wrapper">
+                    <div className="player-name">Kamol (kmln)</div>
+                    <div className='player-car-image'>
+                      <img height={40} src={auto3}  alt="car" />
+                    </div>
+                  </div>
                 </div>
                 <div className="player-speed">
                   45wpm
@@ -47,7 +65,12 @@ const Game = () => {
               </div>
               <div className="racer-wrapper">
                 <div className="player-way">
-                  <p>Moshina</p>
+                  <div className="player-car-wrapper">
+                    <div className="player-name">Akbar (acrdrkbr)</div>
+                    <div className='player-car-image'>
+                      <img height={40} src={auto1}  alt="car" />
+                    </div>
+                  </div>
                 </div>
                 <div className="player-speed">
                   45wpm
